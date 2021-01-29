32768d9e17be57fbcc198363e0430225da12af96
九頭竜八一 <30730238+KuzuryuYaichi@users.noreply.github.com>
2021-01-29 14:13:35 +0800

    Delete 16ch_cpu1/Debug directory

--- 16ch_cpu1/Debug/src/shared.d
+++
@@ -1,6 +0,0 @@
-src/shared.o src/shared.o: ../src/shared.c ../src/shared.h \
- ../../16ch_cpu1_bsp/ps7_cortexa9_1/include/xil_types.h
-
-../src/shared.h:
-
-../../16ch_cpu1_bsp/ps7_cortexa9_1/include/xil_types.h:
